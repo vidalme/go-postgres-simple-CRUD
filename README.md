4ff27f12fa4ff2f443b0a4755a849a4e3e549c28
andre <andrinho@gmail.com>
2024-09-01 12:39:48 -0300

    primeiro push - v1.0 completo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Um simples CRUD 
+### GO backend --/-- PostgreSQL banco de dados
+   
+servirdor dev feito com pacotenet/http nativo do GOlang (so good)
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"loja/routes"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	routes.CarregaRotas()
+	http.ListenAndServe(":8082", nil)
+}
